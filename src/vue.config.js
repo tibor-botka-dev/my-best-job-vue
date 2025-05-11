aa3e2a3bae20287bcdfaafe060304223841b0abf
tibor-botka-dev <botka.tibor.dev@gmail.com>
2025-05-11 11:58:35 +0200

    set prodeuction endpoint

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,6 +1,6 @@
 let host = null;
 if (process.env.NODE_ENV === "production")
-    host = "https://mybestjobapi.azurewebsites.net/"
+    host = "https://my-best-job-api.azurewebsites.net/"
 else
     host = "https://localhost:7183/"
 
